84f5b9ef4e2743aa3dd3ffd4a57bb601cede3386
Noemi Pongracz <noemi.pongracz@softvision.com>
2022-10-03 18:40:06 +0300

    Initial commit

---
+++ frontend/utils/http-client.js
@@ -0,0 +1,63 @@
+class HttpClient {
+    constructor(options = {}) {
+        this.baseUrl = options.baseUrl || "";
+        this.headers = options.headers || {};
+    }
+
+    async fetchJSON(endpoint, options = {}) {
+        const res = await fetch(this.baseUrl + endpoint, {
+            ...options,
+            headers: this.headers
+        });
+
+        if (!res.ok) throw new Error(res.statusText);
+
+        if (options.parseResponse !== false && res.status !== 204)
+            return res.json();
+
+        return undefined;
+    }
+
+
+    setHeader(key, value) {
+        this.headers[key] = value;
+        return this;
+    }
+
+    getHeader(key) {
+        return this.headers[key];
+    }
+
+    get(endpoint, options = {}) {
+        return this.fetchJSON(endpoint, {
+            ...options,
+            method: "GET"
+        });
+    }
+
+    post(endpoint, body, options = {}) {
+        return this.fetchJSON(endpoint, {
+            body: body ? JSON.stringify(body) : undefined,
+            method: "POST"
+        });
+    }
+
+    put(endpoint, body, options = {}) {
+        return this.fetchJSON(endpoint, {
+            body: body ? JSON.stringify(body) : undefined,
+            ...options,
+            method: "PUT"
+        });
+    }
+
+    delete(endpoint, options = {}) {
+        return this.fetchJSON(endpoint, {
+            parseResponse: false,
+            ...options,
+            method: "DELETE"
+        });
+    }
+
+}
+
+export default HttpClient;
